f56ada6f87060ad92027bb6bd851906155584b0d
Elzh@n <elzhan-96@mail.ru>
2024-06-05 07:19:04 +0500

    Initial Commit

---
+++ OnlineStore/OnlineStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreApp.swift
+//  OnlineStore
+//
+//  Created by elzhankk on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
